04498dbe4de5e6c6c495662dd7116fa8180021be
Dan Biagini <d.biagini@yottaa.com>
2020-04-12 09:48:38 -0400

    added hello world node.js server app

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
+const app: express.Application = express();
+
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    // render the index template
+    res.render("index");
+});
+
+// start the express server
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${port}`);
+});
