c7ccce3def5bacbf4418fa3c6f6d824b77e26423
Dan Biagini <d.biagini@yottaa.com>
2020-04-19 17:45:44 -0400

    fix issue with overriding PORT env var in GCP

--- src/google_utils.ts
+++ src/google_utils.ts
@@ -6,7 +6,8 @@ const googleConfig = {
     clientId: process.env.GOOGLE_CLIENT_ID, // e.g. asdfghjkljhgfdsghjk.apps.googleusercontent.com
     clientSecret: process.env.GOOGLE_CLIENT_SECRET, // e.g. _ASDFA%DFASDFASDFASD#FAD-
     redirect: process.env.PROTO + process.env.FQDN + ":" +
-    process.env.PORT + process.env.GOOGLE_REDIRECT_PATH // this must match your google api settings
+        (process.env.NODE_ENV === "development" ? process.env.PORT : '') +
+        process.env.GOOGLE_REDIRECT_PATH // this must match your google api settings
 };
 
 const defaultScope = [
